fb816ff696e0586a31dab9a9aae71dfbf8d8d4ca
Yolo-Arts <Jackteong123@gmail.com>
2026-08-23 20:11:04 +0100

    Main Menu Transitions, Change scene and dynamic UI

---
+++ Assets/Scripts/UI/DynamicUI.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DynamicUI : MonoBehaviour
+{
+    [SerializeField] private float followSpeed = 5f;
+    [SerializeField] private Vector2 offset;
+    [SerializeField] private float maxDistance = 100f;
+
+    private RectTransform rectTransform;
+    private Vector2 startPos;
+    private Vector2 targetPos;
+    private Camera uiCamera;
+
+    void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        startPos = rectTransform.localPosition;
+
+        Canvas canvas = rectTransform.GetComponentInParent<Canvas>();
+        uiCamera = canvas.renderMode == RenderMode.ScreenSpaceOverlay ? null : canvas.worldCamera;
+    }
+
+    void Update()
+    {
+        if (rectTransform.parent == null) return;
+
+        if (Mouse.current == null) return;
+
+        Vector2 mousePosition = Mouse.current.position.ReadValue();
+
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            rectTransform.parent as RectTransform,
+            mousePosition,
+            uiCamera,
+            out targetPos
+        );
+
+        targetPos += offset;
+
+        Vector2 direction = targetPos - startPos;
+        if (direction.magnitude > maxDistance)
+            targetPos = startPos + direction.normalized * maxDistance;
+
+        rectTransform.localPosition = Vector2.Lerp(
+            rectTransform.localPosition,
+            targetPos,
+            Time.deltaTime * followSpeed
+        );
+    }
+}
---
+++ Assets/Scripts/UI/UIFeedback.cs
@@ -0,0 +1,55 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class UIFeedback : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] bool OnHover;
+    [SerializeField] float scaleMult = 1.25f;
+    [SerializeField] float scaleSped = 4f;
+    [SerializeField] float rotateTilt = 3;
+    [SerializeField] float rotateSped = 4f;
+    private Vector3 scale;
+    private Quaternion rotate;
+
+    [SerializeField] AudioClip Hoverclip;
+    [SerializeField] AudioClip Clickclip;
+
+    void Start()
+    {
+        scale = transform.localScale;
+        rotate = transform.rotation;
+    }
+
+    void Update()
+    {
+        if (OnHover)
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, scale * scaleMult, Time.unscaledDeltaTime * (scaleSped * 2));
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotate * Quaternion.Euler(0, 0, rotateTilt), Time.unscaledDeltaTime * (rotateSped * 1.5f));
+        }
+        else
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, scale, Time.unscaledDeltaTime * scaleSped);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotate, Time.unscaledDeltaTime * rotateSped);
+        }
+    }
+
+    public void OnClicked()
+    {
+        // AudioManager.instance.PlaySound(Clickclip);
+        transform.rotation = rotate * Quaternion.Euler(0, 0, -rotateTilt);
+        transform.localScale =  scale * (scaleMult/2);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        OnHover = true;
+        // AudioManager.instance.PlaySound(Hoverclip,0.25f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        OnHover = false;
+    }
+}
